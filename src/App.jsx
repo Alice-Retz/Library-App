f01e150edd247534a6e54967cd685ea66cda6cbc
Alice-Retz <alicegretz@gmail.com>
2021-12-06 16:56:31 -0600

    Book titles are now active links to the book's details

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { NavLink, Route, Switch, BrowserRouter as Router } from 'react-router-dom'
 import BookList from './views/Books/BookList'
 import BookDetail from './views/Books/BookDetail'
 import './App.css'
@@ -5,10 +6,15 @@ import './App.css'
 function App() {
   // TODO: Add routes to books & views
   return (
-    <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
-    </main>
+    <Router>
+      <main className="container">
+        <h1>Library Catalog</h1>
+        <Switch>
+          <Route exact path="/books" component={BookList} />
+          <Route exact path="/books/:id" component={BookDetail} />
+        </Switch>
+      </main>
+    </Router>
   )
 }
 
